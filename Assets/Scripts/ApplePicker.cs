68517aa4d8a6204d29fa2bf7a6f0aa2ddfaed90b
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-09-26 20:03:53 -0400

    Edited ApplePicker Script to Remove Lives when Apples are Dropped

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,15 +10,18 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for(int i = 0; i<numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -28,5 +32,10 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 }
